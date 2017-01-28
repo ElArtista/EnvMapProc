8c685e8b5a883637f833695cf7eea2471d8a3772
TheArtist <agorglouk@gmail.com>
2017-01-28 20:32:26 +0200

    Updated demo link libraries to include PhysFS

--- demo/config.mk
+++ demo/config.mk
@@ -1,5 +1,5 @@
 PRJTYPE = Executable
-LIBS = envmapproc gfxwnd assetloader macu freetype png jpeg tiff zlib glfw glad opencl
+LIBS = envmapproc gfxwnd assetloader physfs macu freetype png jpeg tiff zlib glfw glad opencl
 ifeq ($(OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 winmm ole32 shell32 user32 kernel32
 else
