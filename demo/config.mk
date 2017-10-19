b571315db0aec9bb1df2d7aeb78caf07c2aaf4bd
TheArtist <agorglouk@gmail.com>
2017-10-19 16:16:16 +0300

    Replaced incorrect use of '$(OS)' in build configurations with '$(TARGET_OS)'

--- demo/config.mk
+++ demo/config.mk
@@ -1,6 +1,6 @@
 PRJTYPE = Executable
 LIBS = envmapproc gfxwnd assetloader physfs macu freetype png jpeg tiff zlib glfw glad opencl
-ifeq ($(OS), Windows_NT)
+ifeq ($(TARGET_OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 winmm ole32 shell32 user32 kernel32
 else
 	LIBS += GLU GL X11 Xcursor Xinerama Xrandr Xxf86vm Xi pthread m dl
--- deps/OpenCL/config.mk
+++ deps/OpenCL/config.mk
@@ -2,7 +2,7 @@ PRJTYPE = StaticLib
 SRC = \
 	src/icd.c \
 	src/icd_dispatch.c
-ifeq ($(OS), Windows_NT)
+ifeq ($(TARGET_OS), Windows_NT)
 	SRC += src/icd_windows.c
 else
 	SRC += src/icd_linux.c
